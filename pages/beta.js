b106498a0f126f894fe7191ce13ff36fe44b1bea
christinasofia333 <christinasofia444@gmail.com>
2026-03-30 09:01:55 -0700

    Update beta.js

--- pages/beta.js
+++ pages/beta.js
@@ -8,7 +8,7 @@ export default function BetaAccess() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (password === 'reset2026') {
+    if (password === 'founder2026') {
       sessionStorage.setItem('beta-authenticated', 'true');
       router.push('/');
     } else {
